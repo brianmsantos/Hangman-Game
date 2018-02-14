2ae7259664950611a7c745128d990c943b786e85
Brian Santos <briansantos@Brians-MacBook-Pro.local>
2018-02-13 22:15:58 -0800

    pseudo code

--- assets/game.js
+++ assets/game.js
@@ -1,2 +1,22 @@
-//create an array words to use, TV shows
-var words = ["Game of Thrones", "Friends", "Breaking Bad", "Seinfeld", "Cheers"]
+//create an array words to use, favorite foods 
+var foods = ["Burrito", "Burgers", "Ramen", "Chicken Wings");
+
+//Choose random word from list of words in the array. 
+
+
+
+//Once generator has chosen the random word then it will load the word and provide the number 
+//of spots as a hint to the user. 
+
+
+
+//Click a letter to start, once the letter is chosen it will either generate it in the spot where it belongs or it will make the guess counter decrease. 
+
+
+
+
+//Make sure that once a letter is used it cannot be used again and count against you as a guess. 
+
+
+
+//You should be able to guess the word by typing it out.
