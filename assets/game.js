84f51a77983d05c5d3ef15f4609c11a947f1a53d
Brian Santos <briansantos@Brians-MacBook-Pro.local>
2018-02-13 23:09:21 -0800

    small updates to html and js

--- assets/game.js
+++ assets/game.js
@@ -5,13 +5,16 @@ var foods = ["Burrito", "Burgers", "Ramen", "Chicken Wings");
 
 
 
-//Once generator has chosen the random word then it will load the word and provide the number 
-//of spots as a hint to the user. 
+//Once generator has chosen the random word then it will load the word 
+//and provide the number of spots as a hint to the user. 
+//id="word-blanks" is where this will be stored 
+//letters will load into blank spots and user guesses will decease by 1 --
 
 
 
-//Click a letter to start, once the letter is chosen it will either generate it in the spot where it belongs or it will make the guess counter decrease. 
-
+//Click a letter to start, once the letter is chosen it will either 
+//generate it in the spot where it belongs or it will make the guess counter decrease. 
+//Should this be using the innerHTML?
 
 
 
