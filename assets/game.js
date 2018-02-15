8efd367b5a2b4679a7528491a34a7b4dc524942e
Brian Santos <briansantos@Brians-MacBook-Pro.local>
2018-02-14 18:02:58 -0800

    added first function

--- assets/game.js
+++ assets/game.js
@@ -1,15 +1,26 @@
 //create an array words to use, favorite foods 
 var foods = ["Burrito", "Burgers", "Ramen", "Chicken Wings"];
 
+// //create random food from array
+// var randomNumber = Math.random() * foods.length;
+
+// //spit out from list into one food 
+// var food = random(foods)
 //Choose random word from list of words in the array. 
-var randomFood = foods[Math.floor(Math.random() * foods.length)]
+//var randomFood = foods[Math.floor(Math.random() * foods.length)]
 
 
-function randomFood(foods) {
-    for (let i = 0; i < foods.length; index++) {
-        console.log(foods);
-    }
+function random(arr) {
+    var randomNumber = Math.floor(Math.random() * arr.length);
+    return arr[randomNumber]
+
 }
+
+console.log(random(foods))
+console.log(random(foods))
+console.log(random(["dogs", "trees", 8, 12, 7]))
+
+
 //Once generator has chosen the random word then it will load the word 
 //and provide the number of spots as a hint to the user. 
 
