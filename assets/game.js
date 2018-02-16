41c593c07870c4d5bfa9845601d48781bfa02860
Brian Santos <briansantos@brians-mbp.attlocal.net>
2018-02-15 16:13:24 -0800

    debug

--- assets/game.js
+++ assets/game.js
@@ -12,27 +12,40 @@ function gameStart(params) {
 
 }
 
-//make the game board
 
 //create an array of words to be used 
-var foods = ["Burrito", "Burgers", "Ramen", "Chicken Wings"];
+var foods = ["Burrito", "Burgers", "Ramen", "Wings", "Waffles"];
 
-var answerArray = [];
+var answerWord = random(foods);
+
+var underScores = [];
 
 for (var i = 0; i < foods.length; i++) {
-    answerArray[i] = "_";
+    answerArray[i] = ("_");
 }
 
 var remainingGuesses = 9;
 
 //generate a random word DONE
 function random(arr) {
-    var randomNumber = Math.floor(Math.random() * arr.length);
-    return arr[randomNumber]
+    var randomFood = Math.floor(Math.random() * arr.length);
+    return arr[randomFood]
 }
 console.log(random(foods))
 console.log(random(foods))
 console.log(random(["dogs", "trees", 8, 12, 7]))
+
+
+//make the game board/underscores
+
+function generateUnderscores() {
+    for (var i = 0; i < foods.length; i++) {
+        underScores.push("_");
+    }
+    return underScores();
+}
+
+console.log(generateUnderscores());
 
 //set lives or guesses left 
 
@@ -41,6 +54,7 @@ function lives(param) {
 }
 
 //user inputs, clicks on key events
+
 
 
 //check the input to see if it is in the hangman word
